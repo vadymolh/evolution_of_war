feec5620e26cf31104b7266a025fb90026ecc1ae
Serhii Buniak <master.sergius@gmail.com>
2020-05-18 12:22:58 +0300

    Add MainMenu skeleton

---
+++ src/menu.py
@@ -0,0 +1,42 @@
+"""
+Module designed to handle Main Menu, which appears on start.
+"""
+
+class MainMenu:
+
+    def menu_continue(self):
+        """Fast option to load recent single player game."""
+        pass
+    
+    def menu_single_player(self):
+        # TODO: consider submenu for single player - "new game", "load", etc.
+        pass
+
+    def menu_multi_player(self):
+        # TODO: consider if we are going to implement multiplayer on first release
+        pass
+
+    def menu_settings(self)
+        pass
+
+    def menu_help(self)
+        pass
+
+    def menu_quit(self)
+        pass
+
+    def show_menu(self):
+        """Show all menu options:
+
+        Continue
+        Single Player
+        Multi Player
+        Settings
+        Help
+        Quit
+        """
+        pass
+
+    def click_menu(self):
+        """Perform click action on menu option."""
+        pass
