71ed1afb673e5cc8da1c5aa2e6377835fb0f19eb
Serhii Buniak <master.sergius@gmail.com>
2020-05-26 22:18:50 +0300

    Add gamemap skeleton, add .gitignore

---
+++ src/map.py
@@ -0,0 +1,16 @@
+class GameMap:
+    """ Game map consists of tiles. """
+    pass
+
+
+class Tile:
+    """ Class represents one tile. """
+    # TODO: first release will use square tiles for simplicity
+    pass
+
+
+class MiniMap:
+    """ Minimap shows whole GameMap in tiny size. """
+    # TODO: consider for first release use map which fully fits screen 800x600
+    # thus, minimap doesn't needed to be implemented then
+    pass
---
+++ tests/unit/test_map.py
@@ -0,0 +1,10 @@
+def test_game_map():
+    pass
+
+
+def test_tile():
+    pass
+
+
+def test_minimap():
+    pass
